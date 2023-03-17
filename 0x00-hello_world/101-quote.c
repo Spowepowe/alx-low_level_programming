6f0e5f9312b32f255ac6e45d1307d7dc8f73cfc6
Spowepowe <hannouni-soufiane123321@live.fr>
2023-03-16 18:49:02 -0700

    0x00-hello_world#

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
-int main(void)
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, sizeof(message) - 1);
-	return 1;
+int main(void) {
+    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, str, sizeof(str) - 1);
+    return (1);
 }
