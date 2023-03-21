605e4cae4d3c038074efab9d76ece7f32dab8eec
Spowepowe <hannouni-soufiane123321@live.fr>
2023-03-21 14:02:48 -0700

    print the value of n status greater than 5, is 0 and is not less than 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,14 +18,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	digital = n % 10; /*gets last digital_number*/
+	digit = n % 10; /*gets last digital_number*/
 
-	if (digital > 5)
-		printf("Last digital of %i is %i and is greater than 5\n", n, digital);
-	else if (digital == 0)
-		digital("Last digital of %i is %i and is 0\n", n, digital);
-	else if (digital < 6 && digital != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digital);
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 
 	return (0);
 }
