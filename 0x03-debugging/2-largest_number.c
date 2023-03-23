992b9c20a97ed953b60276b320caee12910a9cbb
Spowepowe <hannouni-soufiane123321@live.fr>
2023-03-23 13:01:53 -0700

    0x03-debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,7 +15,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (a > b && c > a)
+	{
+		largest = c;
+
+	}
+	else if (b > c)
+
 	{
 		largest = b;
 	}
@@ -23,6 +29,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
+
