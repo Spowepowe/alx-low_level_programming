56693c33540593cbd5065e8ecbc40ec07b314408
Spowepowe <hannouni-soufiane123321@live.fr>
2023-03-24 16:49:42 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper
+ *
+ *@c: is a character
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+		return (1);
+	else if (c >= 97 && c < 123)
+		return (0);
+	return (0);
+}
