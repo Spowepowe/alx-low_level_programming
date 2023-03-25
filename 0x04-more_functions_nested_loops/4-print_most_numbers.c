b3a66bd6ac099f5363053a83bd174c0d56fc36de
Spowepowe <hannouni-soufiane123321@live.fr>
2023-03-24 17:11:10 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - check the code
  *
@@ -8,8 +9,8 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 48; a < 58; a++)
-		while (a != 50 && a != 52)
+	while (a = 48; a < 58; a++)
+		if (a != 50 && a != 52)
 			_putchar(a);
 	_putchar('\n');
 }
