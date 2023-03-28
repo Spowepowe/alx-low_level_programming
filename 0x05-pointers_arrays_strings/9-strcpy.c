1d8b0175a63f2462edae0158ad9878e41b4c0c5f
Spowepowe <hannouni-soufiane123321@live.fr>
2023-03-28 15:50:26 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_strcpy : a function that copies the string pointed to by src
+ * char *_strcpy - a function that copies the string pointed to by src
  *
  * @dest: a pointer to a buffer
  *
